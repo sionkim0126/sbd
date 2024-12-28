f74aca9f17a6e8eb8af4d9c4fe7e4ea8dda3425f
sionkim0126 <kimsion000@naver.com>
2024-12-28 22:10:18 +0900

    기본설정

---
+++ src/main/java/sbd/example/springbootdeveloper/TestService.java
@@ -0,0 +1,17 @@
+package sbd.example.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Autowired
+    MemberRepository memberRepository; // ① 빈 주입
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll(); // ② 멤버 목록 얻기
+    }
+}
