d119c693f0dc2c698287d32893ab5ce25ce16ebf
sionkim0126 <kimsion000@naver.com>
2025-01-03 22:56:31 +0900

    TestCode

---
+++ src/test/java/sbd/example/springbootdeveloper/TestControllerTest.java
@@ -0,0 +1,70 @@
+package sbd.example.springbootdeveloper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.http.MediaType; // MediaType도 함께 필요
+
+
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath; // ★ jsonPath 관련
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; // ★ status 관련
+
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TestControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @AfterEach // 테스트 실행 후 실행하는 메서드
+    public void cleanUp() {
+        memberRepository.deleteAll();
+    }
+
+    @DisplayName("getAllMembers: 아티클 조회에 성공한다.")
+    @Test
+    public void getAllMembers() throws Exception {
+        // given
+        final String url = "/test";
+        Member savedMember = memberRepository.save(new Member(1L, "홍길동"));
+
+        // when
+        final ResultActions result = mockMvc.perform(get(url) // ①
+                .accept(MediaType.APPLICATION_JSON)); // ②
+
+        // then
+        result
+                .andExpect(status().isOk()) // ③
+                // ④ 응답의 0번째 값이 DB에 저장한 값과 같은지 확인
+                .andExpect(jsonPath("$[0].id").value(savedMember.getId()))
+                .andExpect(jsonPath("$[0].name").value(savedMember.getName()));
+    }
+
+}
